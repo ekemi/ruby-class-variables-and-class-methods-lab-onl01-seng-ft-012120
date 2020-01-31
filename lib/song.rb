935e2aa58412e423d77a9dc7a593f91ef786b9fb
poignant-writer-1163 <edmundopierre@yahoo.com>
2020-01-31 03:23:27 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,50 @@
+class Song 
+  attr_accessor :name,:genre,:artist
+  
+  @@count = 0
+  
+  @@genres = []
+  @@artists = []
+  
+  
+  def initialize(name,genre,artist)
+    @name = name
+    @genre = genre
+    @artist =artist
+    
+    @@count+=1
+    
+  end
+  
+  def name 
+    
+    @name
+    
+  end 
+  
+  def artist 
+    
+     @genre
+    
+    
+  end
+  
+  def genre
+    
+    @artist
+    
+  end
+  
+  def self.count
+    
+   @@count
+    
+  end
+  
+  def self.artist
+    
+    @@artists.uniq
+  end
+  
+  
+end 
